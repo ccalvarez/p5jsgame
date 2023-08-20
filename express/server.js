35226b2a93ff95bb786428135c2876a935883f32
Carolina Cordero <ccalvarez@gmail.com>
2023-08-20 17:18:30 -0600

    socket.io

--- express/server.js
+++ express/server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const serverless = require('serverless-http');
 
 let app = express();
-let server = app.listen();
+let server = app.listen(3000);
 
 app.use(express.static('public'));
 

c04c59bae41872bea51a745781f7f69ff50f2ebb
Carolina Cordero <ccalvarez@gmail.com>
2023-08-20 17:49:55 -0600

    header cors

--- express/server.js
+++ express/server.js
@@ -9,6 +9,19 @@ let server = app.listen(3000);
 
 app.use(express.static('public'));
 
+app.use((req, res, next) => {
+  if (allowedOrigins.includes(origin)) {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+  }
+
+  res.setHeader(
+    'Access-Control-Allow-Methods',
+    'GET, POST, PUT, PATCH, DELETE'
+  );
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  next();
+});
+
 console.log('My socket server is running!');
 
 let socket = require('socket.io');
